5a42e58a6b22d8f452f9972851e7a8c4355c96c0
ctadas <1226184067@qq.com>
2020-11-25 18:57:32 +0800

    修改登陆验证

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -11,19 +11,23 @@ import os
 import binascii
 
 class WXLoginView(APIView):
-	url ='https://api.weixin.qq.com/sns/jscode2session'
-	code = json.loads(request.body.decode()).get('code')
-	appid = ''
-	secret = ''
-	params = {
-		'appid':appid,
-		'secret':secret,
-		'js_code':code,
-		'grant_type':'authorization_code'
-	}
-	return_request = httprequests.get(url,params)
+	
+	def get_WXopenid(self,code):
+		url ='https://api.weixin.qq.com/sns/jscode2session'
+		#code = json.loads(request.body.decode()).get('code')
+		appid = ''
+		secret = ''
+		params = {
+			'appid':appid,
+			'secret':secret,
+			'js_code':code,
+			'grant_type':'authorization_code'
+		}
+		return_request = httprequests.get(url,params)
 
-	openid = return_request.json()['openid']
+		openid = return_request.json()['openid']
+
+		return openid
 
 	def get_token(self,user):
 		refresh = RefreshToken.for_user(user)
@@ -33,9 +37,11 @@ class WXLoginView(APIView):
 		}
 
 	def get(self,request):
-		print(self.openid)
+		print(request.query_params)
+		openid = get_WXopenid(request.query_params.get('code', None))
+
 		try:
-			user = authenticate(**{'wx_open_id' : self.openid})
+			user = authenticate(**{'wx_open_id' : openid})
 		except ValidationError as e:
 			user = None
 
@@ -55,9 +61,10 @@ class WXLoginView(APIView):
 
 		
 	def post(self,request):
+		openid = get_WXopenid(request.query_params.get('code', None))
 		random_user_name = 'user:%s'%binascii.hexlify(os.urandom(6)).decode()
 		random_password = 'user:%s'%binascii.hexlify(os.urandom(6)).decode()
-		user = User.objects.create_user(username = random_user_name,password = random_password,wx_open_id=self.openid )
+		user = User.objects.create_user(username = random_user_name,password = random_password,wx_open_id=openid )
 		token = self.get_token(user)
 		return Response({
 			'return_type':'success',
